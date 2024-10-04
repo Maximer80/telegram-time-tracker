13875842ae45224cc0754bc156348f973e5aa987
Maxim <inffomax@google.com>
2024-10-04 23:41:59 +0300

    Created TimetrackerApplication

--- src/main/java/com/example/timetracker/TimetrackerApplication.java
+++ src/main/java/com/example/timetracker/TimetrackerApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.example.timetracker;
+package com.example.timetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
